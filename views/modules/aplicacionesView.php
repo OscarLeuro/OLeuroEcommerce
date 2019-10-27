b4f0cdc0133ee20b080e0f6edc08b318aa26af91
Óscar Leuro <alonsoleuro@gmail.com>
2019-10-27 06:14:34 -0500

    Se incluyeron todos los módulos de la WEB y se agresó sistema asíncrono

--- views/modules/aplicacionesView.php
+++ views/modules/aplicacionesView.php
@@ -1,5 +1,39 @@
 <?php
 
-echo 'Aplicaciones';
-
 ?>
+<link rel="stylesheet" href="assets/css/thumb.css">
+
+
+<!--Objeto-->
+<div class="material" onmouseout="document.getElementById('alerta').classList.remove('show')" onmouseover="document.getElementById('alerta').classList.add('show')">
+    <div class="thumb">
+
+    <img src="assets/img/ajax.jpg" alt="">
+    </div>
+    <div class="desc">Curso Ajax</div>
+    <div class="price">$10 <i class="fas fa-shopping-cart"></i></div>
+    <div class="alert" id="alerta">Estados
+            <br>
+            Objeto XmlHttpRequest<br>
+            método OPEN<BR>
+            método SEND<br>
+            Response.Text<br>
+    </div>
+</div>
+
+<!--Objeto-->
+<div class="material" onmouseout="document.getElementById('alerta2').classList.remove('show')" onmouseover="document.getElementById('alerta2').classList.add('show')">
+    <div class="thumb">
+
+    <img src="assets/img/ajax.jpg" alt="">
+    </div>
+    <div class="desc">Curso Ajax</div>
+    <div class="price">$10 <i class="fas fa-shopping-cart"></i></div>
+    <div class="alert" id="alerta2">Estados
+            <br>
+            Objeto XmlHttpRequest<br>
+            método OPEN<BR>
+            método SEND<br>
+            Response.Text<br>
+    </div>
+</div>
